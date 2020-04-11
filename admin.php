1cead82850dca596c757ac622c005c02dbe792d5
Bhojpurikhoj.Com <bhojpurikhoj@gmail.com>
2020-04-12 01:49:24 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,13 +1,12 @@
-
 <?php
 include "config.php";
-
 ?>
 <!doctype html>
 <html>
-    <head><title>Admin - Add Covid19 Details</title>
+    <head><title>Covid 19 Update</title>
+  <!--Import Google Icon Font--><?php include('header.php');?>
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+    
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     
@@ -17,18 +16,15 @@ include "config.php";
 
     <form action="add.php" method="post" class="collection z-depth-1 teal deep-purple white">
            <div class="input-field col s12">
-   <input type="hidden" name = "field2"  class="validate"  id="icon_prefix"/> Title
-<input type="text" name = "field3"  class="materialize-textarea"  id="icon_prefix1"><br/>
-Image Url
-<input type="text" name = "field7"  class="materialize-textarea"  id="icon_prefix1"><br/>
-News<textarea name = "field4"  class="validate"  id="icon_prefix3"  style="height:300px;"></textarea><br/>
-News : <input type="hidden" name = "field5"  class="validate"  id="icon_prefix3" value="<?php echo date("Y-m-d");?>"><?php echo date("Y-m-d");?><br/>
+Total Case
+   <input type="text" name = "field2"  class="validate"  id="icon_prefix"/> <br/>Recover
+<input type="text" name = "field3"  class="validate"  id="icon_prefix1"/> <br/>
+Death<input type="text" name = "field4"  class="validate"  id="icon_prefix3"/><br/>
+Updated : <input type="hidden" name = "field5"  class="validate"  id="icon_prefix3" value="<?php echo date("Y-m-d");?>"><?php echo date("Y-m-d");?><br/>
 
-    <input value="Post" type="submit" style="border:0;padding:3px;" class="badge red white-text">
+    <input type="submit" style="border:0;padding:3px;" class="badge red white-text">
 </form>
-
-
-      </div>
+  </div>
      
 
 <?php
@@ -46,7 +42,7 @@ if ($result = $mysqli->query($query)) {
    ?>
  
       <ul class="collection">
-        <li class="collection-header chip"><img src="https://bhojpurikhoj.com/api/v10/favicon.png" alt="Contact Person">Covid 19 </li>
+        <li class="collection-header chip"><img src="https://bhojpurikhoj.com/api/v10/favicon.png" alt="Contact Person">Covid 19 <a href=https://bhojpurikhoj.com/api/covid19/admin/post.php><span class="badge red white-text chip">Add Post</span></li></a>
         <li class="collection-item">Total Case<span class="badge black white-text">
 <?php echo $total; ?></span></li>
         <li class="collection-item">Recovered<span class="badge green white-text">
@@ -55,12 +51,15 @@ if ($result = $mysqli->query($query)) {
 <?php echo $death; ?></span></li>
       </ul>
       </div>
-     
+      <div class="col s6"><div class="chip">
+   Source : Bhojpurikhoj.Com
+    <i class="close material-icons">close</i>
+  </div>
+      </div>
 
 <?php
  $result->free();
 } 
 ?>
-
 </body>
 </html>
